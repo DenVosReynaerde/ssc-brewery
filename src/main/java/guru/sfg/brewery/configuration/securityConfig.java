d8d72b46f193f7a15ea00b0ff398d157d433cc20
DenVosReynaerde <80472846+DenVosReynaerde@users.noreply.github.com>
2021-08-18 14:21:01 +0200

    configured the index page to not need authorisation

---
+++ src/main/java/guru/sfg/brewery/configuration/securityConfig.java
@@ -0,0 +1,24 @@
+package guru.sfg.brewery.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class securityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests(authorize -> {
+            authorize.antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll();
+        })
+        .authorizeRequests(
+                (requests) -> requests.anyRequest()
+                        .authenticated());
+        http.formLogin();
+        http.httpBasic();
+    }
+}
---
+++ src/test/java/guru/sfg/brewery/web/controllers/IndexControllerTest.java
@@ -0,0 +1,18 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class IndexControllerTest extends baseIT{
+
+    @Test
+    void testGetIndex() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+
+    }
+}
